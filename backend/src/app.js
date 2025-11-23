62799232c795d4b714c25790d98107d870b1d78b
Jilmar01 <jilmar657@hotmail.com>
2025-11-22 20:39:18 -0500

    configuracion local y conexion cliente servidor

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,15 @@
 /* Configuracion principa de express */
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+/* Middlewares */
+app.use(cors());
+
+/* Ruta para enviar un mensaje de prueba */
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'API de NeuroSound funcionando correctamente' });
+});
+
+export default app;
