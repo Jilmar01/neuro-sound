281399702b6b3bb95dab61eb08d420481f1fe5ab
Jilmar01 <jilmar657@hotmail.com>
2025-12-14 09:50:59 -0500

    Depuracion de codigo

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { login, logout } from '../controllers/auth.ctrl.js';
 import { authMiddleware } from '../middleware/auth.mdlw.js';
+import { sendSuccess } from '../utils/response.util.js';
 
 const router = Router();
 
@@ -12,7 +13,7 @@ router.post('/logout', logout);
 
 // Devuelve payload del token (protegida)
 router.get('/me', authMiddleware, (req, res) => {
-  return res.json({ success: true, data: req.user });
+  return sendSuccess(res, req.user, "Usuario tiene una sesion activa", 200);
 });
 
 export default router;
