2a94e76689f28049cc1ee3b455ef89d9630f759e
Jilmar01 <jilmar657@hotmail.com>
2026-01-28 02:07:17 -0500

    Nueva funcion Datos de Usuario Autenticado

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import { HttpError } from "../utils/httpError.js";
 import { generateToken } from "../utils/jwt.util.js";
 import { verifyPassword } from "../utils/password.util.js";
 
@@ -18,7 +19,7 @@ export const authService = async (email, password) => {
     const user = await User.findOne({email}).select('+password');
     const passwordMatch = verifyPassword(password, user.password);
 
-    if(!user || !passwordMatch) throw new Error("Credenciales inválidas");
+    if(!user || !passwordMatch) throw new HttpError("Invalid username or password", 401);
 
     const userObj = user.toObject ? user.toObject() : { ...usuario };
     if (userObj.password) delete userObj.password;
@@ -26,4 +27,22 @@ export const authService = async (email, password) => {
     const token = generateToken(userObj);
 
     return { user: userObj, token};
+}
+
+/**
+ * 
+ * @param {*} userId  - Id del Usuario autenticado
+ * @returns {Promise<Object>} - Datos del usuario
+ */
+export const userAuth = async (userId) => {
+    const user = await User.findById(userId);
+
+    if (!user) {
+        throw new HttpError("User not found", 404);
+    }
+
+    const userObj = user.toObject ? user.toObject() : { ...usuario };
+    if (userObj.password) delete userObj.password;
+
+    return userObj;
 }
