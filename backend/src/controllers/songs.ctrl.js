dfb93b718bdd7677b3052a122d629af4332b509e
Jilmar01 <jilmar657@hotmail.com>
2026-01-28 02:40:29 -0500

    Nueva Funcion de Buscar Canciones en BD

--- backend/src/controllers/songs.ctrl.js
+++ backend/src/controllers/songs.ctrl.js
@@ -1,5 +1,6 @@
-import { findSong, getAllSongs } from "../services/songs.service.js"
+import { findSongsByIds, getAllSongs } from "../services/songs.service.js"
 import { sendError, sendSuccess } from "../utils/response.util.js";
+import { getTrackById } from "../services/spotify.service.js";
 
 export const getSongs = async (req, res) => {
     try {
@@ -12,8 +13,8 @@ export const getSongs = async (req, res) => {
 
 export const getSongById = async (req, res) => {
     try {
-        const { id } = req.body;
-        const song = await findSong("track_id", id);
+        const { track_id } = req.body;
+        const song = await getTrackById(track_id);
         if (!song) {
             return sendError(res, "Canción no encontrada", 404);
         }   
@@ -21,4 +22,14 @@ export const getSongById = async (req, res) => {
     } catch (error) {
         return sendError(res, error.message || "Error interno del servidor", 500);
     }
+}
+
+export const getTracksByIds = async (req, res) => {
+    try {
+        const { track_ids } = req.body; 
+        const track = await findSongsByIds(track_ids);
+        return sendSuccess(res, track, "Canciones obtenidas correctamente", 200);
+    } catch (error) {
+        return sendError(res, "Error al obtener las canciones", error.status, error.message);
+    }  
 }
--- backend/src/routes/songs.routes.js
+++ backend/src/routes/songs.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getSongById, getSongs } from '../controllers/songs.ctrl.js';
+import { getSongById, getSongs, analizeSong, getTracksByIds } from '../controllers/songs.ctrl.js';
 
 const router = Router();
 
@@ -8,5 +8,11 @@ router.get("/all-songs", getSongs);
 
 // Obiene una canción por su ID
 router.post("/track", getSongById);
+
+// Obtiene las canciones por array de IDs
+router.post("/tracks", getTracksByIds);
+
+/* Analiza una cancion por nombre y artista */
+router.post("/analyze-audio", analizeSong);
 
 export default router;
